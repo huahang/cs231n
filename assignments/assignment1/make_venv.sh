e6729b36a437d0dc8912131f85aeff3b49f7ef1d
hans <hliu@me.com>
2024-01-28 20:22:07 +0800

    adjust envs for windows
    Signed-off-by: hans <hliu@me.com>

--- assignments/assignment1/make_venv.sh
+++ assignments/assignment1/make_venv.sh
@@ -3,8 +3,4 @@
 python3 -m venv .venv
 . .venv/bin/activate
 pip install --upgrade pip
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install ipykernel
-pip install future
+pip install -r requirements.txt
