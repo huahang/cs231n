7075d26a89aa2d67a2f93fb88bb7e49ea8f29ed8
hans <hliu@me.com>
2024-02-22 22:32:34 +0800

    coco examples

--- make_env.sh
+++ make_env.sh
@@ -1,37 +1,7 @@
 #!/bin/sh
 
-conda create -n cs231n python=3.8
+conda create -n cs231n python=3.12
 
 conda activate cs231n
 
-conda install \
-  -c pytorch \
-  -c nvidia \
-  -c conda-forge \
-  pytorch \
-  torchvision \
-  jupyterlab \
-  matplotlib \
-  h5py \
-  imageio \
-  scipy \
-  numpy \
-  opencv \
-  pytorch-model-summary \
-  hdf5 \
-  notebook \
-  pycocotools \
-  ipykernel \
-
-pip install fiftyone
-
-OS=$(uname)
-
-if [ "$OS" = "Linux" ]; then
-    echo "This is Linux"
-    conda install -c pytorch -c nvidia -c conda-forge pytorch-cuda
-elif [ "$OS" = "Darwin" ]; then
-    echo "This is Mac"
-else
-    echo "Unknown OS"
-fi
+pip install -r requirements.txt
